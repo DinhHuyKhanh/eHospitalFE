d97d988e5d493d50dff7894b39f3b21075fc2042
nguyenthithuy090501 <nguyenthithuy090501@gmail.com>
2021-11-02 06:01:07 -0700

    sua

--- eHospital/app/src/main/java/com/ktt/ehospital/MainActivity.java
+++ eHospital/app/src/main/java/com/ktt/ehospital/MainActivity.java
@@ -11,35 +11,13 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-    EditText editNgay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-      editNgay = (EditText) findViewById(R.id.editNgay);
-      editNgay.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View view) {
-              ChonNgay();
-          }
-      });
 
-    }
 
-    public void ChonNgay(){
-        Calendar calendar = Calendar.getInstance();
-        int ngay = calendar.get(Calendar.DATE);
-        int thang = calendar.get(Calendar.MONTH);
-        int nam = calendar.get(Calendar.YEAR);
-        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
-                calendar.set(i,i1,i2);
-                SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-                editNgay.setText(dateFormat.format(calendar.getTime()));
-            }
-        },nam,thang,ngay);
-        datePickerDialog.show();
     }
 }
