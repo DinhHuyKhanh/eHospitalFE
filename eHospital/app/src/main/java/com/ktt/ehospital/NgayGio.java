7a32897afc53f21bf1e46054b1c44ae2a40fc626
trungsliver <buiductrung.cva@gmail.com>
2021-11-09 00:44:29 +0700

    duc trung add layout khoa kham + bac si, sua layout ngay gio

--- eHospital/app/src/main/java/com/ktt/ehospital/NgayGio.java
+++ eHospital/app/src/main/java/com/ktt/ehospital/NgayGio.java
@@ -1,29 +1,47 @@
 package com.ktt.ehospital;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class NgayGio extends AppCompatActivity {
+
     EditText editNgay;
+    ImageButton back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ngay_gio);
+
+        back = (ImageButton) findViewById(R.id.imgBack);
         editNgay = (EditText) findViewById(R.id.editNgay);
+
         editNgay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ChonNgay();
             }
         });
+
+        //bắt sự kiện cho nút back trên thanh tiêu đề activity
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NgayGio.this, BacSi.class);
+                startActivity(intent);
+            }
+        });
     }
+
     public void ChonNgay(){
         Calendar calendar = Calendar.getInstance();
         int ngay = calendar.get(Calendar.DATE);
