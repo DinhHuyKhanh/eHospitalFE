58c5863ee8e252842f9c9e20ee5d11cfc52a6f27
Dinh Huy Khanh <khanhdinh141@gmail.com>
2021-11-08 13:59:31 +0700

    update entities

---
+++ eHospital/app/src/main/java/com/ktt/entities/Doctor.java
@@ -0,0 +1,13 @@
+package com.ktt.entities;
+
+public class Doctor extends person{
+    private int department_id;
+
+    public int getDepartment_id() {
+        return department_id;
+    }
+
+    public void setDepartment_id(int department_id) {
+        this.department_id = department_id;
+    }
+}
---
+++ eHospital/app/src/main/java/com/ktt/utils/domain.java
@@ -0,0 +1,49 @@
+package com.ktt.utils;
+
+public class domain {
+    public static String token;
+    public static String urlDepartment;
+    public static String urlAuth;
+    public static String urlDoctor;
+    public static String urlAppointment;
+
+    public static String getToken() {
+        return token;
+    }
+
+    public static void setToken(String token) {
+        domain.token = token;
+    }
+
+    public static String getUrlDepartment() {
+        return urlDepartment;
+    }
+
+    public static void setUrlDepartment(String urlDepartment) {
+        domain.urlDepartment = urlDepartment;
+    }
+
+    public static String getUrlAuth() {
+        return urlAuth;
+    }
+
+    public static void setUrlAuth(String urlAuth) {
+        domain.urlAuth = urlAuth;
+    }
+
+    public static String getUrlDoctor() {
+        return urlDoctor;
+    }
+
+    public static void setUrlDoctor(String urlDoctor) {
+        domain.urlDoctor = urlDoctor;
+    }
+
+    public static String getUrlAppointment() {
+        return urlAppointment;
+    }
+
+    public static void setUrlAppointment(String urlAppointment) {
+        domain.urlAppointment = urlAppointment;
+    }
+}
