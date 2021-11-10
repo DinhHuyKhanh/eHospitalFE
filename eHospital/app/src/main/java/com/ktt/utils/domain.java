4bf7dec41d57745e5191a1758428f29f309171d0
nguyenthithuy090501 <nguyenthithuy090501@gmail.com>
2021-11-10 08:16:17 -0800

    return

--- eHospital/app/src/main/java/com/ktt/utils/domain.java
+++ eHospital/app/src/main/java/com/ktt/utils/domain.java
@@ -3,7 +3,7 @@ package com.ktt.utils;
 public class domain {
     public static String token;
     public static String urlDepartment;
-    public static String urlAuth="http://192.161.1.121:8080/api/auth/";
+    public static String urlAuth="http://192.168.1.5:8080/api/auth/";
     public static String urlDoctor;
     public static String urlAppointment;
 
